69434a0ec7bfc1e89261e2ecee338c96cad52d5b
Pawan Kumar <pawanmkr@192.168.1.4>
2022-04-20 08:24:44 +0530

    initializing repo for the first time

---
+++ LinkedList.java
@@ -0,0 +1,17 @@
+public class LinkedList {
+
+    //this node class is a blueprint of every node that will be created
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# learning + practicing dsa
